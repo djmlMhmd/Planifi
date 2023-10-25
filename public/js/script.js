5f6cd92d016dc85c4dd43506b6f29fe12d41d63a
djamalMhmd <djamaldine-mohamed@live.fr>
2023-10-25 20:48:38 +0200

    add function for show details appointments

--- public/js/script.js
+++ public/js/script.js
@@ -1,63 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
 	var calendarEl = document.getElementById('calendar');
-	var professionalId = 1; // ID du professionnel
-	var availabilityUrl = '/disponibilite/' + professionalId;
-	var reservationsUrl = '/reservations/' + professionalId; // Nouvelle route pour les réservations
 
 	var calendar = new FullCalendar.Calendar(calendarEl, {
 		initialView: 'timeGridWeek',
 		eventSources: [
 			{
-				url: availabilityUrl,
-				method: 'GET',
-			},
-			{
-				url: reservationsUrl,
+				url: 'http://localhost:3000/reservations/1',
 				method: 'GET',
 				extraParams: {
 					custom_param: 'value',
 				},
-				color: 'blue', // Couleur de l'événement
+				color: 'red', // Couleur de l'événement
 				textColor: 'white', // Couleur du texte de l'événement
-				display: 'background', // Affichage de l'événement
-				eventContent: function (info) {
-					if (info.event.extendedProps) {
-						var reservationInfo = info.event.extendedProps;
-						var content = document.createElement('div');
-						content.textContent =
-							'Service: ' + reservationInfo.service_name;
-						content.textContent +=
-							', Utilisateur: ' + reservationInfo.user_fullName;
-						content.textContent +=
-							', Professionnel: ' +
-							reservationInfo.professional_name;
-
-						const userFullName = reservationInfo.user_fullName;
-						if (userFullName) {
-							content.textContent +=
-								", Nom complet de l'utilisateur: " +
-								userFullName;
-						} else {
-							content.textContent +=
-								", Nom complet de l'utilisateur manquant";
-						}
-
-						console.log("Infos de l'événement : ", info);
-						console.log("Contenu de l'événement : ", content);
-
-						return { domNodes: [content] };
-					} else {
-						var content = document.createElement('div');
-						content.textContent =
-							'Données de réservation manquantes ou non définies';
-						return { domNodes: [content] };
-					}
-				},
-				eventDidMount: function (info) {
-					if (info.event?.def?.publicId === 'error') {
-						alert('Erreur lors de la récupération des données');
-					}
-				},
+				//display: 'background', // Affichage de l'événement
 			},
 		],
 
@@ -101,10 +56,29 @@ document.addEventListener('DOMContentLoaded', function () {
 		allDaySlot: false,
 		slotMinTime: '08:00:00', // l'heure de début à 08:00
 		slotMaxTime: '22:00:00', // l'heure de fin à 21:00
+		slotDuration: '00:30:00',
 		slotLabelFormat: {
 			hour: '2-digit',
 			minute: '2-digit',
 			hour12: false,
+		},
+
+		eventClick: function (info) {
+			var service = info.event.extendedProps.reservation.service_id;
+			var client = info.event.title;
+			var professinal =
+				info.event.extendedProps.reservation.professional_name;
+
+			$('#eventModalTitle').text('Détails de lévénement');
+			$('#eventModalContent').html(
+				'Client:' +
+					client +
+					'<br> Service: ' +
+					service +
+					'<br>Professionnel: ' +
+					professinal
+			);
+			$('#eventModal').modal('show');
 		},
 
 		firstDay: 1, // commencer la semaine le lundi
