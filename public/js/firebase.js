a68555403c2dddfee1c41fd90ff59dfe370ae856
djamalMhmd <djamaldine-mohamed@live.fr>
2023-11-02 17:29:37 +0100

    firebase config

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,25 +1,25 @@
-require('dotenv').config();
-import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
+const { initializeApp } = require('firebase/app');
+const { getAnalytics } = require('firebase/analytics');
+const { getStorage } = require('firebase/storage');
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: process.env.API_KEY,
-	authDomain: process.env.AUTH_DOMAIN,
-	projectId: process.env.PROJECT_ID,
-	storageBucket: process.env.STORAGE_BUCKET,
-	messagingSenderId: process.env.MESSAGIN_SENDER_ID,
-	appId: process.env.APP_ID,
-	measurementId: process.env.MEASUREMENT_ID,
+	apiKey: 'AIzaSyAAuB8J_836j7fe1nhJvXg7UUW3giYrakA',
+	authDomain: 'planifi-80c66.firebaseapp.com',
+	projectId: 'planifi-80c66',
+	storageBucket: 'planifi-80c66.appspot.com',
+	messagingSenderId: '450293342385',
+	appId: '1:450293342385:web:1c96304d911010e6974290',
+	measurementId: 'G-NG0TPJGFNG',
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const storage = firebase.storage();
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
 const storageRef = storage.ref();
 
 module.exports = { storage, storageRef };
