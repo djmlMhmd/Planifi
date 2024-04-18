ace44e34eeb84e2c47e4474fd47a010c5cbb9527
MariusVitta <38105695+MariusVitta@users.noreply.github.com>
2024-04-18 21:51:27 +0200

    ajout upload sur firebase

--- routes/profil.js
+++ routes/profil.js
@@ -4,7 +4,12 @@ const router = Router();
 const { getClientsCollection } = require('../db/database');
 const session = require('express-session');
 router.use(express.json());
-const path = require('path');
+//const path = require('path');
+const {getAuth} = require("firebase-admin/auth");
+const {auth} = require("../config/firebase");
+const {getStorage} = require("firebase-admin/storage");
+const {uploadSingle} = require("../middleware/multer");
+const UUID  = require("uuid-v4")
 
 // PROFESSIONAL PROFILE
 
@@ -78,5 +83,84 @@ router.get('/profil/client/:id', async (req, res) => {
 		res.status(500).json({ message: 'Erreur serveur' });
 	}
 });
+
+router.put('/profil/:id/update-profil-picture', uploadSingle, async (req, res) => {
+	// Retrieves the client ID from the session or cookie
+	const clientID = req.session.clientID;
+	const file = req.file
+	const uuid = UUID();
+	let downLoadPath = "https://firebasestorage.googleapis.com/v0/b/planifi-1f28d.appspot.com/o/";
+	let imageUrl = ""
+	if (!clientID) {
+		return res.status(401).json({ message: 'Authentification requise' });
+	}
+	if (!file) {
+		return res.status(400).json({ message: 'Aucun fichier upload' });
+	}
+	req.session.clientID = clientID;
+
+	try {
+		//TODO: rajouter un chemin spécifique pour la sauvegarde dans firebase
+		// mise en ligne de la photo de profil de l'utilisateur
+		const bucket = auth.storage().bucket();
+		const metadata = {
+			metadata: {
+				firebaseStorageDownloadTokens: uuid
+			},
+			contentType: req.file.mimetype,
+			cacheControl: "public, max-age=31536000"
+		}
+
+		const blob = bucket.file(file.originalname)
+		const blobStream = blob.createWriteStream({
+			metadata: metadata,
+			gzip: true
+		})
+
+		blobStream.on("error", err => {
+			console.log(err)
+			return res.status(500).json({ error: "Impossible d'upload la photo"})
+			//return
+		})
+
+		blobStream.on("finish", () => {
+			//const imageUrl = `https://storage/googleapis.com/${bucket.name}/${blob.name}`
+			imageUrl = downLoadPath +
+				encodeURIComponent(file.originalname) +
+				"?alt=media&token=" +
+				uuid;
+			return res.status(201).json({imageUrl})
+
+		})
+		blobStream.end(req.file.buffer)
+
+		//console.log(imageUrl)
+
+		// ajouter la sauvegarde dans la base de données pour l'utilisateur concerné
+		return
+		const client = getClientsCollection();
+		const query = {
+			text: 'SELECT * FROM users WHERE users_id = $1',
+			values: [clientID],
+		};
+
+		const result = await client.query(query);
+
+		if (result.rows.length === 0) {
+			return res
+				.status(404)
+				.json({ message: 'Profil non trouvé' });
+		}
+
+		const { password, creation_date, ...clientProfile } = result.rows[0];
+		res.json(clientProfile);
+	} catch (e) {
+		console.error(
+			'Erreur lors de la récupération du profil:',
+			e.stack
+		);
+		res.status(500).json({ message: 'Erreur serveur' });
+	}
+})
 
 module.exports = router;
