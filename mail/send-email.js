af5a6ed1ef88ec764a31d76ac22e82d721b92689
MariusVitta <38105695+MariusVitta@users.noreply.github.com>
2024-04-28 22:17:56 +0200

    envoie de mail prise de rdv

--- mail/send-email.js
+++ mail/send-email.js
@@ -71,6 +71,114 @@ const sendRegistrationLink = async (userEmail, name, linkRegistration) => {
     }
 }
 
+/**
+ * Fonction qui envoie un mail avec le template de confirmation de rdv à un utilisateur donnée
+ *
+ * @param userEmail mail de l'utilisateur
+ * @param name prénom de l'utilisateur
+ * @param rdvInfos objet avec nom du pro, date et heure du rendez, nom du service
+ * @returns {Promise<void>}
+ */
+const sendConfirmationRendezVousClient = async (userEmail, name, rdvInfos) => {
+    try{
+        /**
+         * [prenom]
+         */
+        const info = await transporter.sendMail({
+            from: `Planifi <${process.env.MAIL_USERNAME}>`,
+            to: userEmail,
+            subject: "Confirmation de rendez-vous",
+            html: `<h3>Bonjour, ${name} nous confirmons votre rendez-vous le ${rdvInfos.date} à ${rdvInfos.heure} avec ${rdvInfos.nom_pro} pour le service ${rdvInfos.service_nom}</h3>`
+        })
+        logLogger(`Message envoyé: ${info.messageId}`, "sendConfirmationRendezVous")
+        logLogger(`Un mail de confirmation de rendez-vous a bien été envoyé à: ${userEmail} avec l'id: ${info.messageId}`, "sendConfirmationRendezVous")
+    }
+    catch (e) {
+        errorLogger(`Erreur lors de l'envoi du mail à ${userEmail}` + e, "sendConfirmationRendezVous")
+    }
+}
+
+/**
+ * Fonction qui envoie un mail avec le template de confirmation de rdv à un utilisateur donnée
+ *
+ * @param userEmail mail de l'utilisateur
+ * @param name prénom de l'utilisateur
+ * @param rdvInfos objet avec nom du pro, date et heure du rendez, nom du service
+ * @returns {Promise<void>}
+ */
+const sendRendezVousPrisPro = async (userEmail, name, rdvInfos) => {
+    try{
+        /**
+         * [prenom]
+         */
+        const info = await transporter.sendMail({
+            from: `Planifi <${process.env.MAIL_USERNAME}>`,
+            to: userEmail,
+            subject: "Nouveau Rendez-vous",
+            html: `<h3>Bonjour, ${name} un rendez-vous à été pris par ${rdvInfos.nom_client} le ${rdvInfos.date} à ${rdvInfos.heure} pour le service ${rdvInfos.service_nom}</h3>`
+        })
+        logLogger(`Message envoyé: ${info.messageId}`, "sendRendezVousPrisPro")
+        logLogger(`Un mail de nouveau de rendez-vous a bien été envoyé à: ${userEmail} avec l'id: ${info.messageId}`, "sendRendezVousPrisPro")
+    }
+    catch (e) {
+        errorLogger(`Erreur lors de l'envoi du mail à ${userEmail}` + e, "sendRendezVousPrisPro")
+    }
+}
+
+/**
+ * Fonction qui envoie un mail avec le template de confirmation de rdv à un utilisateur donnée
+ *
+ * @param userEmail mail de l'utilisateur
+ * @param name prénom de l'utilisateur
+ * @param proInfos objet avec nom du pro, date et heure du rendez, nom du service
+ * @returns {Promise<void>}
+ */
+const sendRendezVousAnnuleClient = async (userEmail, name, proInfos) => {
+    try{
+        /**
+         * [prenom]
+         */
+        const info = await transporter.sendMail({
+            from: `Planifi <${process.env.MAIL_USERNAME}>`,
+            to: userEmail,
+            subject: "Annulation rendez-vous",
+            html: `<h3>Bonjour, ${name} votre rendez-vous du ${proInfos.date} à été annulé par ${proInfos.nom_client} pour le service ${proInfos.service_nom}</h3>`
+        })
+        logLogger(`Message envoyé: ${info.messageId}`, "sendRendezVousAnnuleClient")
+        logLogger(`Un mail de rendez-vous annulé a bien été envoyé à: ${userEmail} avec l'id: ${info.messageId}`, "sendRendezVousAnnuleClient")
+    }
+    catch (e) {
+        errorLogger(`Erreur lors de l'envoi du mail à ${userEmail}` + e, "sendRendezVousAnnuleClient")
+    }
+}
+
+/**
+ * Fonction qui envoie un mail avec le template de confirmation de rdv à un utilisateur donnée
+ *
+ * @param userEmail mail de l'utilisateur
+ * @param name prénom de l'utilisateur
+ * @param proInfos objet avec nom du pro, date et heure du rendez, nom du service
+ * @returns {Promise<void>}
+ */
+const sendConfirmationRendezVousAnnulePro = async (userEmail, name, proInfos) => {
+    try{
+        /**
+         * [prenom]
+         */
+        const info = await transporter.sendMail({
+            from: `Planifi <${process.env.MAIL_USERNAME}>`,
+            to: userEmail,
+            subject: "Confirmation annulation rendez-vous",
+            html: `<h3>Bonjour, ${name} votre rendez-vous du ${proInfos.date} à bien été annulé pour le client ${proInfos.nom_client} pour le service ${proInfos.service_nom}</h3>`
+        })
+        logLogger(`Message envoyé: ${info.messageId}`, "sendConfirmationRendezVousAnnulePro")
+        logLogger(`Un mail de rendez-vous annulé a bien été envoyé à: ${userEmail} avec l'id: ${info.messageId}`, "sendConfirmationRendezVousAnnulePro")
+    }
+    catch (e) {
+        errorLogger(`Erreur lors de l'envoi du mail à ${userEmail}` + e, "sendConfirmationRendezVousAnnulePro")
+    }
+}
+
 const sendRestart = async () => {
     try{
         const info = await transporter.sendMail({
@@ -91,5 +199,9 @@ const sendRestart = async () => {
 module.exports = {
     sendResetPassword,
     sendRegistrationLink,
-    sendRestart
+    sendRestart,
+    sendConfirmationRendezVousClient,
+    sendRendezVousPrisPro,
+    sendRendezVousAnnuleClient,
+    sendConfirmationRendezVousAnnulePro
 }
