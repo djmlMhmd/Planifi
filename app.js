a17cfdc0fa7078ece4c89b002755356bf7659e14
Djamaldine <djamaldine-mohamed@live.fr>
2024-06-09 19:17:21 +0200

    mise en place socket.io

--- app.js
+++ app.js
@@ -88,21 +88,39 @@ app.use('/', messagesRoutes);
 app.use('/', noteRoutes);
 
 // Gestion des connexions WebSocket
+
 io.on('connection', (socket) => {
 	console.log("Un utilisateur s'est connecté");
 
 	socket.on('send_message', async (data) => {
-		const { sender_id, receiver_id, subject, message_body, service_id } =
-			data;
+		console.log(
+			`Tentative d'envoi d'un message avec les données: ${JSON.stringify(
+				data
+			)}`
+		);
+		const { sender_id, receiver_id, subject, message_body } = data;
+
 		const client = getClientsCollection();
 		try {
 			const result = await client.query(
-				`INSERT INTO messages (sender_id, receiver_id, subject, message_body, service_id, sent_at)
-                 VALUES ($1, $2, $3, $4, $5, NOW()) RETURNING *;`,
-				[sender_id, receiver_id, subject, message_body, service_id]
+				`INSERT INTO messages (sender_id, receiver_id, subject, message_body, sent_at)
+                 VALUES ($1, $2, $3, $4, NOW()) RETURNING *;`,
+				[sender_id, receiver_id, subject, message_body]
 			);
 			const message = result.rows[0];
-			socket.to(receiver_id.toString()).emit('new_message', message);
+			message.sent_at = new Date().toISOString();
+			console.log(
+				`Message inséré avec succès: ${JSON.stringify(message)}`
+			);
+
+			socket.broadcast.emit('join_new_room', {
+				sender_id,
+				receiver_id,
+			});
+
+			socket
+				.to(`${sender_id.toString()}-${receiver_id.toString()}`)
+				.emit('new_message', message);
 			socket.emit('message_sent', {
 				status: 'success',
 				message: message,
@@ -120,7 +138,9 @@ io.on('connection', (socket) => {
 	});
 
 	socket.on('join_room', (room) => {
-		socket.join(room);
+		socket.join(room.nom);
+		console.log(socket.adapter.rooms);
+		console.log(`Utilisateur ${socket.id} a rejoint la salle ${room.nom}`);
 	});
 });
 
